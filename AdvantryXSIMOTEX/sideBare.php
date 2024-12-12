6948e1a625e38e668e8643e021b08f8fd2d48532
Othman Turki <o.turki@advantryx.com>
2024-12-12 11:36:51 +0100

    nc

--- AdvantryXSIMOTEX/sideBare.php
+++ AdvantryXSIMOTEX/sideBare.php
@@ -15,7 +15,8 @@
     <li class="nav-item active">
         <a class="nav-link" href="index.php">
             <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
+            <span>Dashboard</span>
+        </a>
     </li>
 
     <!-- Divider -->
@@ -25,85 +26,6 @@
     <div class="sidebar-heading">
         Départements
     </div>
-
-    <!-- Nav Item - Production -->
-    <!-- <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-            aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>Atelier de production</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Chaines de Production:</h6>
-                <?php
-                /* require_once './php/config.php';
-                 $query = "SELECT `prod_line` FROM `init__prod_line` ";
-                 $rslt = $con->query($query);
-
-                 $tab4 = [];
-                 while ($item = $rslt->fetch_assoc()) {
-                     $tab4[] = $item;
-                 }
-                 for ($i = 0; $i < count($tab4); $i++) {
-                     ?>
-                     <a class="collapse-item"
-                         href="dashboardchaine.php?prod_line=<?php echo $tab4[$i]['prod_line'] . "&prod=" . $tab4[$i + 1]['prod_line']; ?>"
-                         target="_blank">
-                         <?php echo $tab4[$i]['prod_line'];
-                 }*/ ?>
-                <?php
-                require_once './php/config.php';
-                require_once './php/configobj.php';
-
-                // Récupérer toutes les chaînes de production depuis la base de données
-                $query = "SELECT prod_line FROM init__prod_line";
-                $rslt = $con->query($query);
-
-                $tab4 = [];
-                if ($rslt) {
-                    while ($item = $rslt->fetch_assoc()) {
-                        $tab4[] = $item;
-                    }
-                } else {
-                    echo "La requête a échoué : " . $con->error;
-                }
-
-                // Vérifier si $tab4 n'est pas vide avant de boucler
-                if (!empty($tab4)) {
-                    $total_lines = count($tab4);
-                    for ($i = 0; $i < $total_lines; $i++) {
-                        $current_prod = $tab4[$i]['prod_line'];
-                        $next_prod_index = ($i + 1) % $total_lines;
-                        $next_prod = $tab4[$next_prod_index]['prod_line'];
-
-
-                        if ($i == $total_lines - 1 && (!isset($_GET['prod']) || !in_array($_GET['prod'], array_column($tab4, 'prod_line')))) {
-                            $next_prod = $tab4[0]['prod_line']; // Redirection vers la première chaîne
-                        }
-
-
-                        $url = "dashboardchaine.php?prod_line=" . urlencode($current_prod);
-                        if ($current_prod != $next_prod) {
-                            $url .= "&prod=" . urlencode($next_prod);
-                        }
-                ?>
-                        <a class="collapse-item" href="<?php echo $url; ?>" target="_blank">
-                            <?php echo htmlspecialchars($current_prod); ?>
-                        </a>
-                <?php
-                    }
-                } else {
-                    echo "Aucune ligne de production trouvée.";
-                }
-                ?>
-
-
-                </a>
-            </div>
-        </div>
-    </li> -->
-
 
     <!-- Nav Item - Méthode -->
     <li class="nav-item">
@@ -117,9 +39,7 @@
                 <h6 class="collapse-header">Bureau de méthode:</h6>
                 <a class="collapse-item" href="objectif.php">Insertion d'objectif</a>
                 <a class="collapse-item" href="ordreDeFab.php">Ordre de fabrication</a>
-                <a class="collapse-item" href="gammeequilibrage.php">Affectation Opération – Box</a>
-                <!-- <a class="collapse-item" href="allpacks.php">Paquets</a>
-                <a class="collapse-item" href="allgamme.php">Modèle-Gamme</a> -->
+                <a class="collapse-item" href="gammeequilibrage.php">Affectation Opération –<br />Box</a>
                 <a class="collapse-item" href="p_operation.php">Opérations</a>
             </div>
         </div>
@@ -134,8 +54,6 @@
         <div id="collapsecq" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Partie de contrôle qualité:</h6>
-                <!-- <a class="collapse-item" href="./controle_quality/dashboard/index.php" target="_blank">Tableau
-                    de bord</a> -->
                 <a class="collapse-item" href="controle.php">Contrôle bout de chaine</a>
                 <a class="collapse-item" href="controlechaine.php">Contrôle sur chaine</a>
                 <a class="collapse-item" href="defautec.php">Liste des défauts</a>
@@ -153,12 +71,8 @@
         <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Position Box:</h6>
-
                 <a class="collapse-item" href="implantation.php">Machine - Box</a>
-
             </div>
-
-
         </div>
     </li>
 
@@ -173,11 +87,9 @@
             <div class="bg-white py-2 collapse-inner rounded">
                 <a class="collapse-item" href="rh.php">Opératrices</a>
                 <a class="collapse-item" href="presence.php">Présence</a>
-                <!-- <a class="collapse-item" href="performanceoperatrice.php">Performances Journalières</a> -->
                 <a class="collapse-item" href="performanceparheure.php">Performances par Heure</a>
             </div>
         </div>
     </li>
-
 </ul>
 <!-- End of Sidebar -->

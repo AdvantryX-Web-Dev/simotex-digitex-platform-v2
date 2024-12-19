01f1cbfb54d29866ee3dd728ae1acd639df6124e
Othman Turki <o.turki@advantryx.com>
2024-12-19 11:23:50 +0100

    nc

--- AdvantryXSIMOTEX/indexprodlinechq.php
+++ AdvantryXSIMOTEX/indexprodlinechq.php
@@ -468,22 +468,6 @@ $engagedQuantities = getEngagedQuantitiesByLast7WorkingDays(
                             </div>
                         </div>
 
-                        <!-- Controle Qualité -->
-                        <!-- <div class="col-xl-6 col-md-6 mb-4">
-                            <div class="card border-left-danger shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">Contrôle Qualité</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800" id="CQ1">
-                                                _
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div> -->
-
                         <!-- Présence -->
                         <div class="col-xl-6 col-md-6 mb-4">
                             <div class="card border-left-dark shadow h-100 py-2">
@@ -539,6 +523,7 @@ $engagedQuantities = getEngagedQuantitiesByLast7WorkingDays(
                             </div>
                         </div>
                     </div>
+
                 </div>
                 <!-- /.container-fluid -->
             </div>

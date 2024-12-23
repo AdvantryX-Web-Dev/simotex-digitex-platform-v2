3072f31f38c3b9d7d78816c27dfc8ce23ab211ab
Othman Turki <o.turki@advantryx.com>
2024-12-23 16:27:44 +0100

    nc

--- AdvantryXSIMOTEX/indexprodlinechq.php
+++ AdvantryXSIMOTEX/indexprodlinechq.php
@@ -450,14 +450,14 @@ $engagedQuantities = getEngagedQuantitiesByLast7WorkingDays(
 
                     <!-- Content Row -->
                     <div class="row">
-                        <!-- Quantité Fabriquée -->
+                        <!-- Quantité Sortie -->
                         <div class="col-xl-6 col-md-6 mb-4">
                             <div class="card border-left-primary shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Quantité Fabriquée
+                                                Quantité Sortie
                                             </div>
                                             <div class="h5 mb-0 font-weight-bold text-gray-800" id="QFAB1">
                                                 <?php echo $producedQuantity; ?>
@@ -496,7 +496,7 @@ $engagedQuantities = getEngagedQuantitiesByLast7WorkingDays(
                             <div class="card shadow mb-4">
                                 <div
                                     class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary">Quantités Fabriquées</h6>
+                                    <h6 class="m-0 font-weight-bold text-primary">Quantités Sorties</h6>
                                 </div>
                                 <div class="card-body">
                                     <div class="chart-area">

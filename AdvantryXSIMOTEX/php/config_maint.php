c2ff4602c7ae791b0f6963234ac6c5b8a737f8f5
Othman Turki <o.turki@advantryx.com>
2025-01-22 12:35:51 +0100

    nc

--- AdvantryXSIMOTEX/php/config_maint.php
+++ AdvantryXSIMOTEX/php/config_maint.php
@@ -1,11 +1,11 @@
 <?php
 
-$con = mysqli_connect("127.0.0.1", "root", "#R3DR&uE3k0RuMk38", "digitex_maint_mahdco");
-// $con = mysqli_connect("db", "root", "#R3DR&uE3k0RuMk38", "digitex_maint_mahdco");
+// $con = mysqli_connect("127.0.0.1", "root", "#R3DR&uE3k0RuMk38", "digitex_maint_mahdco");
+// // $con = mysqli_connect("db", "root", "#R3DR&uE3k0RuMk38", "digitex_maint_mahdco");
 
-if ($con) {
-    // echo "DB connected";
-} else {
-    echo "DB connection is failed";
-    exit();
-}
+// if ($con) {
+//     // echo "DB connected";
+// } else {
+//     echo "DB connection is failed";
+//     exit();
+// }
